8ca9f86a8eedb1fa858fecb99a67531aba1b33d0
Xindi Xu <xindixu@utexas.edu>
2020-05-31 15:03:47 -0500

    Complete mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,63 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// i = 0
+// n = 4
+// while(i < n){
+//   mult = mult + inc
+//   i ++ 
+// }
+
+// n = R0
+// inc = R1
+// i = 0
+// R2 = 0
+
+// LOOP:
+// if i >= n, goto END
+// mult = mult + inc
+// i = i + 1
+// goto LOOP
+
+@R0
+D = M
+@n
+M = D
+
+@R1
+D = M
+@inc
+M = D
+
+@R2
+M = 0
+@i
+M = 0
+
+(LOOP)
+// if i > n, goto END
+// i > n => i - n > 0
+@i
+D = M
+@n
+D = D - M
+@END
+D;JGE
+
+// mult = mult + inc
+@inc
+D = M
+@R2
+M = D + M 
+
+// i = i + 1
+@i
+M = M + 1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
