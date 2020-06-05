25a20370d1558f8fab03838bf706c39934637521
Xindi Xu <xindixu@utexas.edu>
2020-06-05 10:06:13 -0500

    Complete primitive translator

--- 07/VMTraslator.py
+++ 07/VMTraslator.py
@@ -1,6 +1,12 @@
 import sys
 
+operandA = 'R10'
+operandB = 'R11'
 
+arithmetic = {
+    'add': '+',
+
+}
 class Parser:
     def __init__(self, file):
         self.file = file
@@ -17,7 +23,7 @@ class Parser:
             else:
                 self.parseArithmetic(line)
 
-        print(self.parsed)
+        return self.parsed
 
     def cleanUpLine(self, line):
         return line.split('//', 1)[0].strip()
@@ -28,22 +34,124 @@ class Parser:
         self.parsed.append({
             'type': 'C_PUSH' if arr[0] == 'push' else 'C_POP',
             'arg1': arr[1],
-            'arg2': arr[2]
+            'arg2': arr[2],
+            'original': line
         })
 
     def parseArithmetic(self, line):
         self.parsed.append({
             'type': 'C_ARITHMETIC',
-            'arg1': line
+            'arg1': line,
+            'original': line
         })
 
 
 class Translator:
     def __init__(self, instructions):
         self.instructions = instructions
+        self.assembly = []
 
     def translate(self):
-        pass
+        for instruction in self.instructions:
+            if instruction['type'] == 'C_PUSH':
+                self.translatePush(instruction)
+            elif instruction['type'] == 'C_POP':
+                self.translatePop(instruction)
+            elif instruction['type'] == 'C_ARITHMETIC':
+                self.translateArithmetic(instruction)
+
+        self.assembly.append(
+            f"""\
+(END)
+@END
+0;JMP
+"""
+            )
+        return self.assembly
+
+    def getAddress(self, instruction):
+        if instruction['arg1'] == 'constant':
+            return f"""\
+@{instruction['arg2']}
+D = A
+"""
+        else:
+            return f"""\
+@{instruction['arg1']}
+A = M + instruction['arg2']
+D = M
+"""
+
+    def push(self):
+        return f"""\
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+"""
+
+    def pop(self, destination):
+        dest = ''
+        if destination:
+            dest = f"""\
+@{destination}
+M = D
+"""
+        return f"""\
+@SP
+M = M - 1
+A = M
+D = M
+{dest}\
+"""
+
+    def translatePush(self, instruction):
+        self.assembly.append(
+            f"""\
+// {instruction['original']}
+{self.getAddress(instruction)}
+{self.push()}
+"""
+        )
+
+    def translatePop(self, instruction):
+        self.assembly.append(
+            f"""\
+// {instruction['original']}
+{self.pop()}
+{self.getAddress(instruction)}
+"""
+        )
+
+    def translateArithmetic(self, instruction):
+        operator = arithmetic[instruction['arg1']]
+        string = f"""\
+// {instruction['original']}
+{self.pop(operandA)}
+{self.pop(operandB)}
+
+@{operandA}
+D = M
+@{operandB}
+D = D {operator} M
+
+{self.push()}
+"""
+        self.assembly.append(
+            f"""\
+// {instruction['original']}
+{self.pop(operandA)}
+{self.pop(operandB)}
+
+@{operandA}
+D = M
+@{operandB}
+D = D {operator} M
+
+{self.push()}
+"""
+        )
 
 
 def main():
@@ -51,12 +159,13 @@ def main():
     file = open(pathToFile, 'r')
     instructions = Parser(file).parse()
     assembly = Translator(instructions).translate()
-    # pathToOutputFile = pathToFile.split('.')[0]
-    # try:
-    #     outputFile = open(f"{pathToOutputFile}.asm", 'x')
-    # except FileExistsError:
-    #     outputFile = open(f"{pathToOutputFile}.asm", "w")
-    # outputFile.writelines(assembly)
+
+    pathToOutputFile = pathToFile.split('.')[0]
+    try:
+        outputFile = open(f"{pathToOutputFile}.asm", 'x')
+    except FileExistsError:
+        outputFile = open(f"{pathToOutputFile}.asm", 'w')
+    outputFile.writelines(assembly)
 
 
 main()
