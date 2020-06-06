4d7634f2d5d9acc01e2737e80b3bc4e668e71e9c
Xindi Xu <xindixu@utexas.edu>
2020-06-05 22:08:14 -0500

    Pass StackTest

---
+++ 07/StackArithmetic/StackTest/StackTest.asm
@@ -0,0 +1,788 @@
+
+// push constant 17
+@17
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 17
+@17
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// eq
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE0
+D;JNE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE0
+0;JMP
+
+(FALSE0)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE0)
+
+// push constant 17
+@17
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 16
+@16
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// eq
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE1
+D;JNE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE1
+0;JMP
+
+(FALSE1)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE1)
+
+// push constant 16
+@16
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 17
+@17
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// eq
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE2
+D;JNE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE2
+0;JMP
+
+(FALSE2)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE2)
+
+// push constant 892
+@892
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 891
+@891
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// lt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE3
+D;JGE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE3
+0;JMP
+
+(FALSE3)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE3)
+
+// push constant 891
+@891
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 892
+@892
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// lt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE4
+D;JGE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE4
+0;JMP
+
+(FALSE4)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE4)
+
+// push constant 891
+@891
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 891
+@891
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// lt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE5
+D;JGE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE5
+0;JMP
+
+(FALSE5)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE5)
+
+// push constant 32767
+@32767
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 32766
+@32766
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// gt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE6
+D;JLE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE6
+0;JMP
+
+(FALSE6)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE6)
+
+// push constant 32766
+@32766
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 32767
+@32767
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// gt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE7
+D;JLE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE7
+0;JMP
+
+(FALSE7)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE7)
+
+// push constant 32766
+@32766
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 32766
+@32766
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// gt
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@FALSE8
+D;JLE
+
+D = -1
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+@CONTINUE8
+0;JMP
+
+(FALSE8)
+D = 0
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(CONTINUE8)
+
+// push constant 57
+@57
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 31
+@31
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// push constant 53
+@53
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// add
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M + D
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+// push constant 112
+@112
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// sub
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M - D
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+// neg
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+
+@R13
+D = -M
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+// and
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M & D
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+// push constant 82
+@82
+D = A
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+// or
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+@SP
+M = M - 1
+A = M
+D = M
+@R14
+M = D
+
+
+@R13
+D = M
+@R14
+D = M | D
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+
+// not
+@SP
+M = M - 1
+A = M
+D = M
+@R13
+M = D
+
+
+@R13
+D = !M
+
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+
+(END)
+@END
+0;JMP
